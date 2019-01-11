b21ced634760b34199fe0c4d132304c7c09a06f3
O W Williams <nterface@imac.local>
2019-01-11 01:43:10 -0600

    Removed paste error

--- InterestingInfoUITests/InterestingInfoUITests.swift
+++ InterestingInfoUITests/InterestingInfoUITests.swift
@@ -25,8 +25,7 @@ class InterestingInfoUITests: XCTestCase {
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-    testPhotoNodePressed()
-testTableOpenNodePressed()
+
     func testExample() {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
